2497f0f85ed5ce22ab8fa6f0c561aeabdeb9b7ac
Faisal Ahmed <faisalbaloch09876@gmail.com>
2024-03-20 08:11:31 +0500

    Online classes and projects, completed the simple calculator project
    completed the first project, about a simple calculator with nodejs and typescript.

---
+++ Ramadan Online Classes/night2/Simple-Calculator/guide.ts
@@ -0,0 +1,21 @@
+/*
+* npx (node package execute)
+it allows execute commands from packages in the npm registry without installing them first.
+
+* npm init
+ command for initializing the project by creating the package.json file.
+
+* tsc --init
+ creates the tsconfig.json file, used to configure the typescript.
+
+* change the tsconfig
+ same as the file in this project.
+ 
+* add "type": "module",
+ inside the package.json
+
+* search npm inquirer.
+ A collection of common interactive command line user interfaces.
+
+
+ */
---
+++ Ramadan Online Classes/night2/Simple-Calculator/index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number:", type: "number", name: "firstNumber" },
+    {
+        message: "Enter second number:",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//  conditional statement
+switch (answer.operator) {
+    case "Addition":
+        console.log("your value is", answer.firstNumber + answer.secondNumber);
+        break;
+    case "Multiplication":
+        console.log("your value is", answer.firstNumber * answer.secondNumber);
+        break;
+    case "Division":
+        console.log("your value is", answer.firstNumber / answer.secondNumber);
+        break;
+    case "Subtraction":
+        console.log("your value is", answer.firstNumber - answer.secondNumber);
+        break;
+}
