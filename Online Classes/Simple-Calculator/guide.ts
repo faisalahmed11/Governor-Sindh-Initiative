c52a3bd9aca0acac1ef9f5bc1e38164eccb7b647
Faisal Ahmed <faisalbaloch09876@gmail.com>
2024-03-23 13:40:10 +0500

    Changed folders position.
    this is to make the overall folder structure good.

--- Online Classes/Simple-Calculator/guide.ts
+++ Online Classes/Simple-Calculator/guide.ts
@@ -18,7 +18,7 @@ it allows execute commands from packages in the npm registry without installing 
  A collection of common interactive command line user interfaces.
 
 * add #! /usr/bin/env node
- at the top of the main file, for making it executable.
+ at the top of the main file, for making it executable, it is also called as shibang.
 
 * add "bin": "index.js",
  inside the package.json
@@ -40,4 +40,10 @@ run this command on the terminal to publish the package on the npmjs.com
 
 . to rebublish the package you need to increase the version of the package.
 
+
+, npm Versions
+1.0.0
+1.0.1 for bug fixes
+1.1.0 for minor features
+2.0.0 for major features.
  */
